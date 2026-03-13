8a51e44071595c13a5579728fef4805feb8e84a9
Harrison H. Vaughn Reed <harrisonhvreed@gmail.com>
2026-03-13 12:51:48 -0400

    Adding visibility to failed db cred attempts

--- alembic/env.py
+++ alembic/env.py
@@ -16,11 +16,11 @@ config = context.config
 # Pull credentials from the environment
 DATABASE_URL = os.environ.get("DATABASE_URL")
 if not DATABASE_URL:
-    DB_HOST = os.environ.get("DB_HOST")
-    DB_PORT = os.environ.get("DB_PORT")
-    DB_NAME = os.environ.get("DB_NAME")
-    DB_USER = os.environ.get("DB_USER")
-    DB_PASSWORD = os.environ.get("DB_PASSWORD")
+    DB_HOST = os.environ.get("DB_HOST", ValueError("DB_HOST environment variable is not set"))
+    DB_PORT = os.environ.get("DB_PORT", ValueError("DB_PORT environment variable is not set"))
+    DB_NAME = os.environ.get("DB_NAME", ValueError("DB_NAME environment variable is not set"))
+    DB_USER = os.environ.get("DB_USER", ValueError("DB_USER environment variable is not set"))
+    DB_PASSWORD = os.environ.get("DB_PASSWORD", ValueError("DB_PASSWORD environment variable is not set"))
     DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 config.set_main_option("sqlalchemy.url", DATABASE_URL)
--- etl/database.py
+++ etl/database.py
@@ -1,13 +1,15 @@
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+load_dotenv()
 
 # Pull credentials from the environment
-DB_HOST = os.environ.get("DB_HOST", Warning("DB_HOST environment variable is not set"))
-DB_PORT = os.environ.get("DB_PORT", Warning("DB_PORT environment variable is not set"))
-DB_NAME = os.environ.get("DB_NAME", Warning("DB_NAME environment variable is not set"))
-DB_USER = os.environ.get("DB_USER", Warning("DB_USER environment variable is not set"))
-DB_PASSWORD = os.environ.get("DB_PASSWORD", Warning("DB_PASSWORD environment variable is not set")) # nosec B105
+DB_HOST = os.environ.get("DB_HOST", ValueError("DB_HOST environment variable is not set"))
+DB_PORT = os.environ.get("DB_PORT", ValueError("DB_PORT environment variable is not set"))
+DB_NAME = os.environ.get("DB_NAME", ValueError("DB_NAME environment variable is not set"))
+DB_USER = os.environ.get("DB_USER", ValueError("DB_USER environment variable is not set"))
+DB_PASSWORD = os.environ.get("DB_PASSWORD", ValueError("DB_PASSWORD environment variable is not set"))
 
 DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
